07aa83f2c55fd4a6e2d9bc2b32f7c51a0dfa5978
demo <demo@example.com>
2026-09-14 04:42:38 +0000

    ホームボタンのロゴを20%縮小(128px→102px)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2012,7 +2012,7 @@ export default function Home() {
               <img
                 src="/home-icon.png"
                 alt="ホーム"
-                className="h-32 w-32 max-w-none object-contain"
+                className="h-[102px] w-[102px] max-w-none object-contain"
               />
             </button>
           </div>
